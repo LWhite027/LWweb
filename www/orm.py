360fdfe9163f8c8884e808e461667ca7ba3d5010
LWhite <861606393@qq.com>
2017-08-12 19:53:40 +0800

    orm.py

--- www/orm.py
+++ www/orm.py
@@ -58,3 +58,32 @@ async def execute(sql, args, autocommit=True):
             raise
         return affected
 
+#定义所有ORM映射的基类Model：
+class Model(dict, metaclass = ModelMetaclass):
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+        def __setattr__(self, key, value):
+            self[key] = value
+        
+        def getValue(self, key):
+            return getattr(self, key, None)
+
+        def ValueOrDefault(self, key):
+            value = getattr(self, key ,None)
+            if value is None:
+                field = self.__mappings__[key]
+                if field.default is not None:
+                    value = field.default() if callable(field.default) else field.default
+                    logging.debug('using default value for %s: %s' % (key, str(value)))
+                    setattr(self, key, value)
+            return value
+
+
+
